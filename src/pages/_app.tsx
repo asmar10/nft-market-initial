4ef21f165ba6c7e58a6e3b9b6a308631a857d808
unknown <asmarhasan14@gmail.com>
2022-10-07 19:54:54 +0500

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import "react-toastify/dist/ReactToastify.css";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import {SignerProvider} from "state/signer";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SignerProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SignerProvider>
+  );
+};
+
+export default MyApp;
